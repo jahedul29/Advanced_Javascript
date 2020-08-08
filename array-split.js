acc5944d52e4f25ae3cc3e774821ccdb7017f42a
jahedul29 <jahedulh1@gmail.com>
2020-08-08 13:31:27 +0600

    Array slice, spice and join

---
+++ array-split.js
@@ -0,0 +1,17 @@
+const numbers = [1, 2, 4, 6, 7];
+
+//Array slicing
+const part = numbers.slice(1, 4);
+
+console.log(removed);
+console.log(numbers);
+
+//Array splice
+const removed = numbers.splice(2, 2);
+
+console.log(removed);
+console.log(numbers);
+
+//Array join
+const together = numbers.join("/");
+console.log(together);
