7d43e7f66141fb971b6f3d30668bbddf7e984985
jahedul29 <jahedulh1@gmail.com>
2020-08-08 10:45:55 +0600

    Truth vs False

---
+++ truth-false.js
@@ -0,0 +1,21 @@
+/**
+ * Number 0 is false
+String “0” is false.
+An empty string is false.
+Undefined is false.
+NaN is false.
+Null is false.
+Whitespace in a string is true.
+Empty array and object is true.
+False in string is true.
+
+ */
+
+const age = 4;
+const name = "Solaiman Khan";
+
+if (name) {
+    console.log("Condition is true");
+} else {
+    console.log("Condition is false");
+}
